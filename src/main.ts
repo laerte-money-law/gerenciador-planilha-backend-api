f43cef037aa8cb12c2667213505dfdd514b0c936
Maycon Huanca <mayconfelipe164@gmail.com>
2026-02-04 09:58:12 -0300

    feat(in progress): add deploy pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
+
+console.log("URL FRONTEND");
+console.log(process.env.FRONTEND_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
   app.setGlobalPrefix('api');
