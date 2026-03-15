ba7cb1dbb2ba6ff593ce734cac47496fa6255b3b
Maycon Huanca <mayconfelipe164@gmail.com>
2026-03-14 21:25:42 -0300

    feat: Extend user controller access to client role and update user response DTO and mapper to include client data and a structured team object.

--- src/users/model/mapper/user.mapper.ts
+++ src/users/model/mapper/user.mapper.ts
@@ -6,6 +6,8 @@ import { CreateUserDto } from "../dto/create-user.dto";
 import { CreateUserResponseDto } from "../dto/create-user.response.dto";
 import { UserResponseDto } from "../dto/user-response.dto";
 
+import { ClientOutputDto } from "../../../client/model/dto/client.ouput.dto";
+
 export const usertMapperToEntity = (userDto: CreateUserDto): User => {
     return plainToInstance(User, {
         name: userDto.name,
@@ -16,7 +18,7 @@ export const usertMapperToEntity = (userDto: CreateUserDto): User => {
     });
 }
 
-export const userMapperToBodyResponse = (user: User): CreateUserResponseDto=> {
+export const userMapperToBodyResponse = (user: User): CreateUserResponseDto => {
     return plainToInstance(CreateUserResponseDto, {
         name: user.name,
         email: user.email,
@@ -28,10 +30,11 @@ export const userMapperToResponseDto = (user: User): UserResponseDto => {
         id: user.id,
         name: user.name,
         email: user.email,
-        team: {
+        team: user.team ? {
             id: user.team.id,
             name: user.team.name
-        },
+        } : null,
+        client: user.client ? ClientOutputDto.fromEntity(user.client) : null,
         role: user.role
     });
 }
